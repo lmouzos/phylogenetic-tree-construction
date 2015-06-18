83168d80b841e426950ff95fea72c7cc16e1b023
moujan <moujan21@gmail.com>
2015-06-18 15:46:58 +0200

    Upload Source Code for the project

---
+++ baseSub.h
@@ -0,0 +1,12 @@
+#ifndef BASESUB_H_
+#define BASESUB_H_
+
+#include "GraphClass.h"
+
+std::vector<std::string> cleanData(std::vector<std::string> Set);
+
+#endif 
+
+
+
+
---
+++ spacedWordFrequency.h
@@ -0,0 +1,7 @@
+#ifndef SPACEDWORDFREQUENCY_H_
+#define SPACEDWORDFREQUENCY_H_
+
+double** spacedWordFrequency(std::map<int, std::map<int, std::map<std::string,int> > > f);
+
+#endif 
+
